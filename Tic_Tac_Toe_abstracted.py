cc5987eb701ff813e6ba2346a2b09e35af2f564f
tyler <TylerEngleVFX@Gmail.com>
2025-03-19 15:31:02 -0700

    omg I think I finally fixed it. It was just an indenting issue. But now it's causing a bug where it doesn't restart the game

--- Tic_Tac_Toe_abstracted.py
+++ Tic_Tac_Toe_abstracted.py
@@ -31,9 +31,9 @@ def playAgain():
             print("invalid input")
             continue
 
-
 def tic_tac_toe():
     b = ["   " for _ in range(9)] #initializes the empty list of symbols
+    #list of all possible win combos in the form of sets
     winset = [
     {0,1,2},
     {3,4,5},
@@ -54,20 +54,20 @@ def tic_tac_toe():
         won = False
         tie = False
         current_player = (turn%2)
-        players = [" X ", " O "]
+        symbols = [" X ", " O "]
         while True:
             try:
                 pinp= int(input(f"Player {current_player+1}, enter your move: "))-1
                 if not (8 >= pinp >= 0):
                     print("Input must be between 1 and 9")
                     continue
-                elif checkNotBlank(b,pinp):
+                elif checkNotBlank(b,pinp): #checks if the space is already taken
                     continue
                 break
             except ValueError:
                 print("\nINVALID INPUT, TRY AGAIN…")
                 continue
-        b[pinp] = players[current_player]
+        b[pinp] = symbols[current_player] #adds the current symbol to the board
         print("\n")
         printBox(b)
         print()
@@ -77,25 +77,26 @@ def tic_tac_toe():
         #     print(win)
         # print(setlist[current_player])
         for win in winset:
-            if win.issubset(setlist[current_player]):
+            print(f"win is {win} and current input set is {setlist[current_player]}") #diagnostic
+            if win.issubset(setlist[current_player]): #THIS IS THE BUGGY LINE OF CODE
                 won=True
-            if won:
-                print(f"\nP{current_player+1} wins!")
-                if playAgain():
-                    b = ['   ' for _ in range(9)]
-                    printBox(b) #prints fresh box
-                    inplist=[[],[]] #resets input lists to being empty
-                    break
-                else: sys.exit()
-            tie = "   " not in b
-            if tie:
-                print("\nTIE!")
-                if playAgain():
-                    b = ['   ' for _ in range(9)]
-                    printBox(b) #prints fresh box
-                    inplist=[[],[]] #resets input lists to being empty
-                    break
-                else: sys.exit()
+        if won:
+            print(f"\nP{current_player+1} wins!")
+            if playAgain():
+                b = ['   ' for _ in range(9)]
+                printBox(b) #prints fresh box
+                inplist=[[],[]] #resets input lists to being empty
+                break
+            else: sys.exit()
+        tie = "   " not in b
+        if tie:
+            print("\nTIE!")
+            if playAgain():
+                b = ['   ' for _ in range(9)]
+                printBox(b) #prints fresh box
+                inplist=[[],[]] #resets input lists to being empty
+                break
+            else: sys.exit()
         turn+=1
 
 if __name__ == "__main__":
