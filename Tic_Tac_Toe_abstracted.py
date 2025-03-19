446a54f06522ec8a9984a5ab3ca768dbacd23ece
tyler <TylerEngleVFX@Gmail.com>
2025-03-19 14:39:03 -0700

    simplified, but still needing double win bug fix

--- Tic_Tac_Toe_abstracted.py
+++ Tic_Tac_Toe_abstracted.py
@@ -1,156 +1,102 @@
 #! python3
 #A Game of Tic Tac Toe
-#Still need to simplify code
+#Still need to bugs where it doesn't detect duplicate wins
 import sys 
 
-b = ["   " for _ in range(9)]
-
-def printBox():
+def printBox(b):
     print(f"{b[0]}|{b[1]}|{b[2]}   =>   1,2,3")
     print("---+---+---")
     print(f"{b[3]}|{b[4]}|{b[5]}   =>   4,5,6")
     print("---+---+---")
     print(f"{b[6]}|{b[7]}|{b[8]}   =>   7,8,9")
 
-def checkNotBlank(i):
+def checkNotBlank(b, i):
     if b[i] != "   ":
         print("Space already taken")
         return True
     
-# def playAgain():
-#     while True:
-#         try:
-#             again = input("Would you like to play again? (Y/N): ").upper()
-#         except:
-#             print("invalid input")
-#         if again == "Y":
-#             p1list=[] 
-#             p2list=[]
-#             b = ['   ' for _ in range(9)]
-#             print("\nNEW GAME!")
-#             printBox() #prints fresh box
-#             break
-#         elif again == "N":
-#             print("Thanks for playing!\nExiting program...")
-#             sys.exit()
-#         else: 
-#             print("invalid input")
-#             continue
-    
-wins = [
-[0,1,2],
-[3,4,5],
-[6,7,8],
-[0,4,8],#diag
-[2,4,6],
-[0,3,6],#vert
-[1,4,7],
-[2,5,8],
-]
-
-#empty lists that will hold the player's inputs as the game goes on
-p1list=[] 
-p2list=[]
-
-print("\nWELCOME TO TIC TAC TOE 3.0!!!")
-printBox() #prints initial box
-#The Game Begins...
-while True:
-    #Player 1 Turn
+def playAgain():
     while True:
         try:
-            p1inp= int(input("Player 1, enter your move: "))-1
-            if not (8 >= p1inp >= 0):
-                print("Input must be between 1 and 9")
-                continue
-            elif checkNotBlank(p1inp):
-                continue
-            break
-        except ValueError:
-            print("\nINVALID INPUT, TRY AGAIN…")
+            again = input("Would you like to play again? (Y/N): ").upper()
+        except:
+            print("invalid input")
+        if again == "Y":
+            print("\nNEW GAME!")
+            return True
+        elif again == "N":
+            print("Thanks for playing!\nExiting program...")
+            return False
+        else: 
+            print("invalid input")
             continue
-    b[p1inp]=" X "
-    print("\n")
-    printBox()
-    print()
-    p1list.append( int(p1inp) ) # holds a list of all P1 input
-    setp1 = set(p1list) # converts P1 input list into a set
-    for win in wins:
-        setwin = set(win)
-        setp1 = set(p1list)
-        print(setwin, setp1)
-        won = setwin.issubset(setp1)
-        tie = "   " not in b
-    # p1Same = p1Set & setWins # finds the elements that are the same between wins and P1 set and puts it into a set
-    # if p1Same in setWins:
-        if won:
-            print("\nP1 wins!")
-            while True:
-                try:
-                    again = input("Would you like to play again? (Y/N): ").upper()
-                except:
-                    print("invalid input")
-                if again == "Y":
-                    p1list=[] 
-                    p2list=[]
-                    b = ['   ' for _ in range(9)]
-                    print("\nNEW GAME!")
-                    printBox() #prints fresh box
-                    break
-                elif again == "N":
-                    print("Thanks for playing!\nExiting program...")
-                    sys.exit()
-                else: 
-                    print("invalid input")
-                    continue
-        elif tie:
-            print("\nTIE!")
-            playAgain()
-    #Player 2 Turn
+
+
+def tic_tac_toe():
+    b = ["   " for _ in range(9)] #initializes the empty list of symbols
+    winset = [
+    {0,1,2},
+    {3,4,5},
+    {6,7,8},
+    {0,4,8},#diag
+    {2,4,6},
+    {0,3,6},#vert
+    {1,4,7},
+    {2,5,8},
+    ]
+    inplist=[[],[]] #empty lists that will hold the player's inputs as the game goes on
+    print("\nWELCOME TO TIC TAC TOE 3.0!!!")
+    printBox(b) #prints initial box
+    turn = 0
+    #The Game Begins...
     while True:
-        try:
-            p2inp= int(input("Player 2, enter your move: "))-1
-            if not (8 >= p2inp >= 0):
-                print("Input must be between 1 and 9")
-                continue
-            elif checkNotBlank(p2inp):
-                continue
-            break
-        except ValueError:
-            print("\nINVALID INPUT, TRY AGAIN…")
-            continue
-    b[p2inp]=" O "
-    print("\n")
-    printBox()
-    print()
-    p2list.append( int(p2inp) ) # holds a list of all P1 input
-    setp2 = set(p2list) # converts P1 input list into a set
-    for win in wins:
-        setwin = set(win)
-        setp2 = set(p2list)
-        print(setwin, setp2)
-        won = setwin.issubset(setp2)
-        tie = "   " not in b
-        if won:
-            print("\nP2 wins!")
-            while True:
-                try:
-                    again = input("Would you like to play again? (Y/N): ").upper()
-                except:
-                    print("invalid input")
-                if again == "Y":
-                    p1list=[] 
-                    p2list=[]
-                    b = ['   ' for _ in range(9)]
-                    print("\nNEW GAME!")
-                    printBox() #prints fresh box
-                    break
-                elif again == "N":
-                    print("Thanks for playing!\nExiting program...")
-                    sys.exit()
-                else: 
-                    print("invalid input")
+        #New Turn
+        won = False
+        tie = False
+        current_player = (turn%2)
+        players = [" X ", " O "]
+        while True:
+            try:
+                pinp= int(input(f"Player {current_player+1}, enter your move: "))-1
+                if not (8 >= pinp >= 0):
+                    print("Input must be between 1 and 9")
                     continue
-        elif tie:
-            print("\nTIE!")
-            playAgain()
+                elif checkNotBlank(b,pinp):
+                    continue
+                break
+            except ValueError:
+                print("\nINVALID INPUT, TRY AGAIN…")
+                continue
+        b[pinp] = players[current_player]
+        print("\n")
+        printBox(b)
+        print()
+        inplist[current_player].append( int(pinp) ) # holds a list of all player input
+        setlist=[set(inplist[0]),set(inplist[1])] #list containing the 2 sets based off the 2 input lists
+        # for win in winset:
+        #     print(win)
+        # print(setlist[current_player])
+        for win in winset:
+            if win.issubset(setlist[current_player]):
+                won=True
+            if won:
+                print(f"\nP{current_player+1} wins!")
+                if playAgain():
+                    b = ['   ' for _ in range(9)]
+                    printBox(b) #prints fresh box
+                    inplist=[[],[]] #resets input lists to being empty
+                    break
+                else: sys.exit()
+            tie = "   " not in b
+            if tie:
+                print("\nTIE!")
+                if playAgain():
+                    b = ['   ' for _ in range(9)]
+                    printBox(b) #prints fresh box
+                    inplist=[[],[]] #resets input lists to being empty
+                    break
+                else: sys.exit()
+        turn+=1
+
+if __name__ == "__main__":
+    tic_tac_toe()
